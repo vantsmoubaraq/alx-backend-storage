c2c8fe06c7c63a61460786c4014773375e39df54
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-03 21:34:13 +0300

    mysql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- lists all bands with Glam rock as their main style
 -- displays columns band_name and lifespan
-SELECT DISTINCT `band_name`, `split` - `formed` AS `lifespan`
+SELECT DISTINCT `band_name`, IFNULL(`split`, 2023) - `formed` AS `lifespan`
 FROM `metal_bands`
 WHERE style LIKE "%Glam rock%" ORDER BY `lifespan` DESC;
