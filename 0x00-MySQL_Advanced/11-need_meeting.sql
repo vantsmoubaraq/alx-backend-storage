ffe5f986557d63aacd732c47790c31a70fe7316a
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-04 11:05:19 +0300

    mysql_advanced

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,6 @@
+--  creates a view need_meeting that lists all
+-- students that have a score under 80
+
+CREATE VIEW need_meeting AS
+SELECT name FROM students
+    WHERE score < 80 AND (last_meeting IS NULL OR DATE_SUB(NOW(), INTERVAL 1 MONTH));
