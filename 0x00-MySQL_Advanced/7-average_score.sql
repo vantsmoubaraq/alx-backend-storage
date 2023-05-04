ab95ca56a2f463156795c9dc0561a3fdc9aa9ab9
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-04 08:25:40 +0300

    mysql_advanced

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,12 @@
+-- computes and store the average score for a student.
+-- Takes in user id
+
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE average FLOAT;
+    SELECT AVG(score) INTO average FROM corrections
+    WHERE corrections.user_id = user_id;
+    UPDATE users SET average_score = average WHERE id = user_id;
+END//
