26a7ea0e7f440374b9bf18de8d91679274f9ddaf
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-04 11:09:19 +0300

    mysql_advanced

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -5,4 +5,4 @@ DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting
 AS
 SELECT name FROM students 
-    WHERE score < 80 and (last_meeting IS NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH));
+    WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH));
