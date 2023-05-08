d0cb784d2ce6faa231374f4cdf0df5d026d540a0
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-08 18:37:12 +0300

    NoSQL

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 
+"""
+Module prints docs according to method
+"""
+
 from pymongo import MongoClient
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
-    print(f"{nginx_collection.count_documents({})} logs")
-    print("Methods")
+    print("{} logs".format(nginx_collection.count_documents({})))
+    print("Methods:")
     for item in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        print("\t method {}: {}".format(item,
+        print("\tmethod {}: {}".format(item,
               nginx_collection.count_documents({"method": item})))
-    print("{} status check".format(nginx_collection.count_documents({})))
+    print("{} status check".format(nginx_collection.count_documents(
+          {"method": "GET", "path": "/status"})))
     print("IPs:")
     result = {}
     for item in nginx_collection.find():
