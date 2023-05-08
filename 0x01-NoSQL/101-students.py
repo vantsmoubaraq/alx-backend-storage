589cd0a054009f8a95db428ef5ea0fffc66a5862
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-08 17:10:02 +0300

    NoSQL

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""returns all students sorted by average score"""
+
+
+def top_students(mongo_collection):
+    """returns all students sorted by average score"""
+    new_collection = []
+    for doc in mongo_collection.find():
+        count = 0
+        total_score = 0
+        for item in doc["topics"]:
+            total_score += item["score"]
+            count += 1
+        if (count >= 1):
+            doc["averageScore"] = total_score / count
+        else:
+            doc["averageScore"] = 0
+
+        new_collection.append(doc)
+
+    print(new_collection)
+    return sorted(new_collection, key=lambda x:
+                  x["averageScore"], reverse=True)
