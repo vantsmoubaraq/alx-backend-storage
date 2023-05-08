468750270f31edfeb8d784703a688871695eaf32
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-08 14:11:51 +0300

    NoSQL

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -8,4 +8,5 @@ nginx_collection = client.logs.nginx
 print(f"{nginx_collection.count_documents({})} logs")
 print("Methods")
 for item in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-    print("\t method {}: {}".format(item, nginx_collection.count_documents({"method" : item})))
+    print("\t method {}: {}".format(item,
+          nginx_collection.count_documents({"method": item})))
