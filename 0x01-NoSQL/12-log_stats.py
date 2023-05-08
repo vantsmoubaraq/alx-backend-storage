34582822f8853f106524daed7cbc7b309da30b87
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-08 18:30:30 +0300

    NoSQL

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
-    print(f"{nginx_collection.count_documents({})} logs")
+    print("{} logs".format(nginx_collection.count_documents({})))
     print("Methods:")
     for item in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
         print("\tmethod {}: {}".format(item,
